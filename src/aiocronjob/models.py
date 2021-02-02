773bf400c7c8d8789d3b12c395d5b396fdcf1658
devtud <devtud@gmail.com>
2021-02-02 12:39:36 +0200

    Add models

---
+++ src/aiocronjob/models.py
@@ -0,0 +1,53 @@
+import datetime
+from asyncio.tasks import Task
+
+from dataclasses import dataclass, field
+
+from .typing import Literal, Coroutine, Callable, Optional, List
+
+JobStatus = Literal[
+    "registered", "running", "finished", "pending", "cancelled", "failed"
+]
+
+
+@dataclass
+class JobInfo:
+    name: str
+    last_status: str
+    enabled: bool
+    crontab: str
+    created_at: datetime.datetime = None
+    started_at: datetime.datetime = None
+    stopped_at: datetime.datetime = None
+    next_run_in: int = None
+
+
+@dataclass
+class JobDefinition:
+    name: str
+    async_callable: Callable[[], Coroutine]
+    enabled: bool = True
+    crontab: Optional[str] = None
+
+
+@dataclass
+class RunningJob:
+    job_definition: JobDefinition
+    asyncio_task: Task
+    since: datetime.datetime
+
+
+@dataclass
+class JobLog:
+    event_name: Literal[
+        "job_registered", "job_started", "job_failed", "job_finished", "job_cancelled"
+    ]
+    job_definition: JobDefinition
+    error: str = None
+    timestamp: int = field(default_factory=lambda: datetime.datetime.now().timestamp())
+
+
+@dataclass
+class State:
+    created_at: datetime.datetime
+    jobs_info: List[JobInfo]
